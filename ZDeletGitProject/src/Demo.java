47902eec3f24406c7f2e1e2d5562620f70bd8470
MANISH <MANISH@DESKTOP-NPV50CU>
2021-06-03 09:30:06 +0530

    Submitted by me

---
+++ ZDeletGitProject/src/Demo.java
@@ -0,0 +1,29 @@
+
+public class Demo 
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println(" Hello ");
+		System.out.println(" Hello  DemoGit ");
+		
+		Demo  dm=new Demo();
+		dm.add();
+		dm.multiple();
+		
+	
+	}
+	
+	
+	 public void add()
+	 {
+		 System.out.println(" Addition method ");
+	 }
+	 public void multiple()
+	 {
+		 System.out.println(" Multiply method ");
+	 }
+	
+	
+
+}
